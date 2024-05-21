cec3b91a2b2604809ef34744cd7560627b331961
unknown <tranmaster000@gmail.com>
2024-05-21 12:45:50 +0700

    first commit

---
+++ job-seeking-be/job-seeking-be/src/main/java/com/jobseeking/jobseekingbe/JobSeekingBeApplication.java
@@ -0,0 +1,13 @@
+package com.jobseeking.jobseekingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeekingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSeekingBeApplication.class, args);
+	}
+
+}
---
+++ job-seeking-be/job-seeking-be/src/test/java/com/jobseeking/jobseekingbe/JobSeekingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobseeking.jobseekingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeekingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
